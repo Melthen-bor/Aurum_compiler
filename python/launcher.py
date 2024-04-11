06ef45cd30b8ee69e3c2b13bdc17de9ce500a381
Malthein Bor <151883748+Melthen-bor@users.noreply.github.com>
2024-04-10 20:24:39 -0700

    update python version

--- python/launcher.py
+++ python/launcher.py
@@ -2,6 +2,11 @@ import compiler
 import preprocessor
 a=input()
 b=preprocessor.preprocessor()
-b.directives(a+" no")
+d=a.split(" ")
+a=d[0]
+if d[1]=="-obj":
+    b.directives(a+" true")
+elif d[1]=="-0":
+    b.directives(a+" false")
 c=compiler.compilerGold()
 c.run(a)
